013485f09b7ed5b3aa34a92dc5d426e64004b3e0
ajengwidya <ajengwidya55@gmail.com>
2025-03-26 10:21:52 +0700

    Rearrange files

--- api/server.js
+++ api/server.js
@@ -6,27 +6,27 @@ import cors from "cors";
 
 import productRoutes from "./routes/product.route.js";
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 const app = express();
 
 // Middleware to connect to the database before handling requests (for Vercel)
-app.use(async (request, response, next) => {
-  try {
-    // Ensure the DB is connected
-    await connectDB();
+// app.use(async (request, response, next) => {
+//   try {
+//     // Ensure the DB is connected
+//     await connectDB();
 
-    // Continue to the next middleware or route handler
-    next();
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    response.status(500).json({ message: "Database connection failed" });
-  }
-});
+//     // Continue to the next middleware or route handler
+//     next();
+//   } catch (error) {
+//     console.error("Error connecting to the database:", error);
+//     response.status(500).json({ message: "Database connection failed" });
+//   }
+// });
 
 // Use value from the env, otherwise 5000
 // Uncomment for localhost
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
@@ -42,24 +42,29 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-// Check if the env is production
+/** 
+ * Check if the env is production
+ * This block of code will serve the files
+ * from frontend folder as well
+ * so both folders are accessible in port 5000
+ */
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 // Export the app for Vercel
-export default app;
+// export default app;
 
 /**
  * The first param is the port
  * The second param is the callback function
  * Commented out for deploying to Vercel, uncomment for localhost
  */
-// app.listen(PORT, () => {
-//   connectDB();
-//   console.log(`Server started at http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started at http://localhost:${PORT}`);
+});
